c198d7818f8915fdbe987fd5b842588049869bf2
chrisyhlee <leechr8393@gmail.com>
2023-01-31 10:36:50 -0800

    completed api-endpoints minus stretch

--- app/controllers/toons_controller.rb
+++ app/controllers/toons_controller.rb
@@ -1,2 +1,16 @@
 class ToonsController < ApplicationController
+    def index
+        toons = Toon.all
+        render json: toons
+    end
+    def create
+        toon = Toon.create(toon_params)
+        render json: toon
+    end
+
+
+    private
+    def toon_params
+        params.require(:toon).permit(:name, :age, :enjoys_doing, :image)
+    end
 end
