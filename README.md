006fe5914ee8058ca15dea7a506387825ae4a829
chrisyhlee <leechr8393@gmail.com>
2023-02-03 12:22:47 -0800

    fixed update request spec

--- README.md
+++ README.md
@@ -1,24 +1,38 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+* request spec update
 
-Things you may want to cover:
+# let(:toon) do
+#   Toon.create!(    
+#       id: 1,  
+#       name: 'Buster',
+#       age: 4,
+#       enjoys_doing: 'Meow Mix, and plenty of sunshine.',
+#       image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+#   )
+# end
+  
+# # context 'with valid parameters' do
+# let(:params) do 
+#   {
+#     toon: {
+#       id: 1,
+#       name: 'NEW BUSTER',
+#       age: 4,
+#       enjoys_doing: 'Meow Mix, and plenty of sunshine.',
+#       image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+#     }
+#   }
+# end
 
-* Ruby version
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+# it 'will update a whole toon info' do
+#   # put "/api/v1/toons/#{toon.id}", params: params
+#   # expect { toon.reload }.to change(toon, :name).to("NEW BUSTER")
+#   put "/toons/1", params: { id: toon, toon: params }
+#   expect(toon.name).to eq('NEW BUSTER')
+#   expect(response.status).to eq 200
+#   toon.reload!
+# end
+    
+# end
--- spec/requests/toons_spec.rb
+++ spec/requests/toons_spec.rb
@@ -137,7 +137,6 @@ RSpec.describe "Toons", type: :request do
 
       toon_updated_params = {
         toon: {
-          id: 1,
           name: 'Not Buster',
           age: 4,
           enjoys_doing: 'Meow Mix, and plenty of sunshine.',
@@ -145,48 +144,11 @@ RSpec.describe "Toons", type: :request do
         }
       }
 
-      patch "/toons/#{toon_updated_params.toon.id}", params: toon_updated_params
+      patch "/toons/#{toon.id}", params: toon_updated_params
       expect(response).to have_http_status(200)
-      toon = Toon.first
-      expect(toon.name).to eq 'Not Buster'
+      new_toon = Toon.first
+      expect(new_toon.name).to eq 'Not Buster'
     end
   end
   
 end
-
-
-
-# let(:toon) do
-#   Toon.create!(    
-#       id: 1,  
-#       name: 'Buster',
-#       age: 4,
-#       enjoys_doing: 'Meow Mix, and plenty of sunshine.',
-#       image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
-#   )
-# end
-  
-# # context 'with valid parameters' do
-# let(:params) do 
-#   {
-#     toon: {
-#       id: 1,
-#       name: 'NEW BUSTER',
-#       age: 4,
-#       enjoys_doing: 'Meow Mix, and plenty of sunshine.',
-#       image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
-#     }
-#   }
-# end
-
-
-# it 'will update a whole toon info' do
-#   # put "/api/v1/toons/#{toon.id}", params: params
-#   # expect { toon.reload }.to change(toon, :name).to("NEW BUSTER")
-#   put "/toons/1", params: { id: toon, toon: params }
-#   expect(toon.name).to eq('NEW BUSTER')
-#   expect(response.status).to eq 200
-#   toon.reload!
-# end
-    
-# end
